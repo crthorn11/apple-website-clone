c7e743b82dbbde10885b97f1d681688c7c744a6a
Cody T <crthorn11@gmail.com>
2024-12-14 10:05:45 -0500

    Finished Model.jsx file by creating a button with yes or no statements to change state and color of button at the bottom with iphone screen size

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -88,8 +88,14 @@ gsap.to('#heading', { y: 0, opacity: 1 })
                                     onClick={() => setModel(item)} />
                             ))}
                         </ul>
-                        <button>
-
+                        <button className='size-btn-container'>
+                            {sizes.map(({ label, value }) => (
+                                <span key={label} className='size-btn'
+                                style={{ backgroundColor: size ===
+                                    value ? 'white' : 'transparent', color: size === value ? 'black' : 'white'}} onClick={() => setSize (value)}>
+                                    {label}
+                                </span>
+                            ))}
                         </button>
                     </div>
                 </div>
